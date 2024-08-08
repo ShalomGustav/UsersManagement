2c7ae7f947251e985680067f8e459b1b0889139d
Максим Сергеевич Фатихов <maximvaryagov@gmail.com>
2024-08-08 13:14:51 +0300

    GetUserByLogin добавлен

--- UsersManagement/Controllers/UserController.cs
+++ UsersManagement/Controllers/UserController.cs
@@ -14,7 +14,23 @@ namespace UsersManagement.Controllers
             return View();
         }
 
-        /// добавить GetUserByLogin
+        [HttpPost("user/{login}")]
+        public async Task<ActionResult> GetUserByLogin(string login)
+        {
+            if (string.IsNullOrEmpty(login))
+            {
+                throw new ArgumentNullException(nameof(login));
+            }
+
+            var user = await _userService.GetByLogin(login);
+
+            if (user == null)
+            {
+                throw new NullReferenceException(nameof(user));
+            }
+
+            return Ok(user);
+        }
 
         public UserController(UserService userService)
         {
